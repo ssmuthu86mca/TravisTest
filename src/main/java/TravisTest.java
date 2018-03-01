52da384f6ebabcd41f88cf1f3fb77b7ba6c2ff0d
somasu04 <sudalaimuthu.somasundaram.ap@nielsen.com>
2018-03-02 01:17:17 +0530

    First Travis Commit

---
+++ src/main/java/TravisTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author somasu04
+ *
+ */
+public class TravisTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Travis Test Class");
+	}
+	
+	public void test(){
+		System.out.println("First Travis Test");
+	}
+}
